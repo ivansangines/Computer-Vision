b3dc79e4295ea985efd9e31b6b68bd6e1ca3e636
ivansangines <35933947+ivansangines@users.noreply.github.com>
2019-08-18 12:13:16 -0400

    CannyHarris

---
+++ Assignment3_ComputerVision_SANGINES/CannyHarris/CannyHarris/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace CannyHarris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.load_btn = new System.Windows.Forms.Button();
+            this.canny_btn = new System.Windows.Forms.Button();
+            this.harris_btn = new System.Windows.Forms.Button();
+            this.original_img = new System.Windows.Forms.PictureBox();
+            this.modified_img = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.original_img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.modified_img)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // load_btn
+            // 
+            this.load_btn.Location = new System.Drawing.Point(24, 47);
+            this.load_btn.Name = "load_btn";
+            this.load_btn.Size = new System.Drawing.Size(75, 23);
+            this.load_btn.TabIndex = 0;
+            this.load_btn.Text = "Load Image";
+            this.load_btn.UseVisualStyleBackColor = true;
+            this.load_btn.Click += new System.EventHandler(this.load_btn_Click);
+            // 
+            // canny_btn
+            // 
+            this.canny_btn.Location = new System.Drawing.Point(24, 157);
+            this.canny_btn.Name = "canny_btn";
+            this.canny_btn.Size = new System.Drawing.Size(75, 23);
+            this.canny_btn.TabIndex = 1;
+            this.canny_btn.Text = "Canny";
+            this.canny_btn.UseVisualStyleBackColor = true;
+            this.canny_btn.Click += new System.EventHandler(this.canny_btn_Click);
+            // 
+            // harris_btn
+            // 
+            this.harris_btn.Location = new System.Drawing.Point(24, 197);
+            this.harris_btn.Name = "harris_btn";
+            this.harris_btn.Size = new System.Drawing.Size(75, 23);
+            this.harris_btn.TabIndex = 2;
+            this.harris_btn.Text = "Harris";
+            this.harris_btn.UseVisualStyleBackColor = true;
+            this.harris_btn.Click += new System.EventHandler(this.harris_btn_Click);
+            // 
+            // original_img
+            // 
+            this.original_img.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.original_img.Location = new System.Drawing.Point(142, 47);
+            this.original_img.Name = "original_img";
+            this.original_img.Size = new System.Drawing.Size(368, 304);
+            this.original_img.TabIndex = 3;
+            this.original_img.TabStop = false;
+            // 
+            // modified_img
+            // 
+            this.modified_img.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.modified_img.Location = new System.Drawing.Point(553, 47);
+            this.modified_img.Name = "modified_img";
+            this.modified_img.Size = new System.Drawing.Size(368, 304);
+            this.modified_img.TabIndex = 4;
+            this.modified_img.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(252, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ORIGINAL IMAGE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(706, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "MODIFIED IMAGE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 522);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.modified_img);
+            this.Controls.Add(this.original_img);
+            this.Controls.Add(this.harris_btn);
+            this.Controls.Add(this.canny_btn);
+            this.Controls.Add(this.load_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.original_img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.modified_img)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button load_btn;
+        private System.Windows.Forms.Button canny_btn;
+        private System.Windows.Forms.Button harris_btn;
+        private System.Windows.Forms.PictureBox original_img;
+        private System.Windows.Forms.PictureBox modified_img;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Assignment3_ComputerVision_SANGINES/CannyHarris/CannyHarris/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace CannyHarris
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bm;
+        Image<Gray, Byte> img;
+        Mat resImage = new Mat();
+        Image<Gray, float> harrisImage = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void load_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "jpeg files (*.jpg)|*.jpg|(*.gif)|gif||";
+            if (DialogResult.OK == dialog.ShowDialog())
+            {
+                
+                
+                img = new Image<Gray, Byte>(dialog.FileName);
+
+                original_img.BackgroundImage = img.ToBitmap();
+                
+            }
+        }
+
+        private void canny_btn_Click(object sender, EventArgs e)
+        {
+            
+            CvInvoke.Canny(img, resImage, 50, 200);
+            modified_img.BackgroundImage = resImage.Bitmap;
+
+        }
+
+        private void harris_btn_Click(object sender, EventArgs e)
+        {
+            harrisImage = new Image<Gray, float>(img.Size);
+            CvInvoke.CornerHarris(img, harrisImage, 2, 3, 0.02);
+            modified_img.BackgroundImage = harrisImage.ToBitmap();
+        }
+    }
+}
